2f083baffa9b088c19cf11a2fe6d8bd85827f0d9
ABetancourt03 <neutralzgamer@gmail.com>
2022-01-30 15:16:06 -0430

    Fix para que se pueda instalar la PWA

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import App from './App.js';
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('./sw.js')
     .then((reg) => console.log('Registro de SW exitoso', reg))
     .catch((err) => console.warn('Error al tratar de registrar el sw', err));
 }
