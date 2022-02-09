3e4cf844ad916803134ff654fd33e38c1e25d80e
ABetancourt03 <neutralzgamer@gmail.com>
2022-02-08 22:47:02 -0430

    Agregando clase Bloodmoon al div de la ronda

--- js/App.js
+++ js/App.js
@@ -1,52 +1,54 @@
-import {$} from './helpers/getElements.js';
-import AxieEnergyCount from './components/AxieEnergyCount.js';
+import { $ } from './helpers/getElements.js'
+import AxieEnergyCount from './components/AxieEnergyCount.js'
 
-const $root = $('.root');
-const $body = document.body;
+const $root = $('.root')
+const $body = document.body
 
 export default function App() {
-  $root.innerHTML += AxieEnergyCount();
+  $root.innerHTML += AxieEnergyCount()
 }
 
-export let energy = 3;
-let roundNumber = 1;
+export let energy = 3
+let roundNumber = 1
 
-document.addEventListener('click', (e) => {
-  const $round = $('.round');
-  const $actualEnergy = $('.actual-energy');
+document.addEventListener('click', e => {
+  const $round = $('.round')
+  const $actualEnergy = $('.actual-energy')
 
   if (e.target.textContent === '-1') {
-    energy--;
+    energy--
     if (energy < 0) {
-      energy = 0;
+      energy = 0
     }
-    $actualEnergy.innerHTML = `${energy}/10`;
+    $actualEnergy.innerHTML = `${energy}/10`
   }
   if (e.target.textContent === '+1') {
-    energy++;
+    energy++
     if (energy > 10) {
-      energy = 10;
+      energy = 10
     }
-    $actualEnergy.innerHTML = `${energy}/10`;
+    $actualEnergy.innerHTML = `${energy}/10`
   }
   if (e.target.textContent === 'Next Round') {
-    roundNumber++;
-    energy++;
-    energy++;
+    roundNumber++
+    energy++
+    energy++
     if (energy > 10) {
-      energy = 10;
+      energy = 10
     }
-    $actualEnergy.innerHTML = `${energy}/10`;
-    $round.innerHTML = `Round ${roundNumber}`;
+    $actualEnergy.innerHTML = `${energy}/10`
+    $round.innerHTML = `Round ${roundNumber}`
   }
   if (roundNumber === 10) {
-    $body.classList.add('bloodmoon-course');
+    $body.classList.add('bloodmoon-course')
+    $round.classList.add('bloodmoon-course')
   }
   if (e.target.textContent === 'New Game') {
-    roundNumber = 1;
-    energy = 3;
-    $actualEnergy.innerHTML = `${energy}/10`;
-    $round.innerHTML = `Round ${roundNumber}`;
-    $body.classList.remove('bloodmoon-course');
+    roundNumber = 1
+    energy = 3
+    $actualEnergy.innerHTML = `${energy}/10`
+    $round.innerHTML = `Round ${roundNumber}`
+    $body.classList.remove('bloodmoon-course')
+    $round.classList.remove('bloodmoon-course')
   }
-});
+})
